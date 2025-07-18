1284cb7a5fee37f6427b426a33a4da7629b13e40
S Fowler <94100649+SNFowler@users.noreply.github.com>
2025-07-18 13:32:57 +1000

    Created partial skeleton for adjoint class

--- adjoint_sim_sf/adjointsolver.py
+++ adjoint_sim_sf/adjointsolver.py
@@ -88,6 +88,145 @@ class SymmetricTransmonBuilder(DesignBuilder):
         gui = MetalGUI(design)
         gui.rebuild()
     
+    
+class AdjointOptimiser:
+    """
+    Encapsulates the adjoint optimisation process.
+
+    @TODO: The forward and backward solvers don't necessarily need to be explicitly computing the full array of E field values if they're already contained in the sim_sParams object. 
+    """
+    
+    def __init__(self, initial_parameters: np.array, lr: float, builder: DesignBuilder):
+        self.builder = builder
+        self.design = None
+        self.current_parameters = self.initialise_parameters()
+        self.lr = lr # learning rate
+
+        self.fwd_field_sParams = None
+        self.adjoint_field_sParams = None
+        self.grad = None
+
+        self.fwd_field_data = None
+        self.adjoint_field_data = None
+
+        self.fwd_source_location =      [-25e-3, 2e-3, 100e-6]
+        self.adjoint_source_location =  [0,0, 100e-6]
+
+
+    def fwd_calculation(self):
+        assert self.design
+
+        self.design.rebuild()
+
+        #Instantiate a COMSOL model
+        cmsl = COMSOL_Model('leModel')
+
+        #Create simulations to setup - in this case capacitance matrix and RF s-parameter
+        sim_sParams = COMSOL_Simulation_RFsParameters(cmsl, adaptive='None')
+
+        #(A) - Initialise model from Qiskit-Metal design object: design
+        cmsl.initialize_model(design, [sim_sParams], bottom_grounded=True)
+
+        cmsl.add_metallic(1, threshold=1e-12, fuse_threshold=1e-10)
+        cmsl.add_ground_plane()
+        cmsl.fuse_all_metals()
+
+        sim_sParams.create_port_JosephsonJunction('junction', L_J=4.3e-9, C_J=10e-15, R_J=10e3)
+
+        # sim_sParams.add_surface_current_source_region("dielectric", 0.5)
+        # sim_sParams.add_surface_current_source_region("metals", 10e-6, 2)
+
+        # edp_pts = ShapelyEx.get_points_uniform_in_polygon(poly1, 0.01,0.01)
+        # for cur_pt in edp_pts:
+        #     x, y = cur_pt[0]*0.001, cur_pt[1]*0.001 #Converting from mm to m
+        #     sim_sParams.add_electric_point_dipole([x,y, 1e-6], 1, [0,0,1])
+
+        sim_sParams.add_electric_point_dipole(self.fwd_source_location, 1, [0,1,0])
+        # sim_sParams.add_electric_point_dipole([0,0,0], 0, [0,1,0])
+
+        cmsl.fine_mesh_around_comp_boundaries(['pad1', 'pad2'], minElementSize=10e-6, maxElementSize=50e-6)
+
+        cmsl.build_geom_mater_elec_mesh(skip_meshing=True, mesh_structure='Fine')
+
+        sim_sParams.set_freq_values([8.0333e9])
+        # cmsl.plot()
+        sim_sParams.run()
+        
+        self.fwd_field_data = sim_sParams.eval_fields_over_mesh()
+
+
+    def adjoint_calculation(self):
+        assert self.design
+        assert self.fwd_field_data
+
+        self.design.rebuild()
+
+        #Instantiate a COMSOL model
+        cmsl = COMSOL_Model('leModel')
+
+        #Create simulations to setup - in this case capacitance matrix and RF s-parameter
+        sim_sParams = COMSOL_Simulation_RFsParameters(cmsl, adaptive='None')
+
+        #(A) - Initialise model from Qiskit-Metal design object: design
+        cmsl.initialize_model(design, [sim_sParams], bottom_grounded=True)
+
+        cmsl.add_metallic(1, threshold=1e-12, fuse_threshold=1e-10)
+        cmsl.add_ground_plane()
+        cmsl.fuse_all_metals()
+
+        sim_sParams.create_port_JosephsonJunction('junction', L_J=4.3e-9, C_J=10e-15, R_J=10e3)
+
+        # sim_sParams.add_surface_current_source_region("dielectric", 0.5)
+        # sim_sParams.add_surface_current_source_region("metals", 10e-6, 2)
+
+        # edp_pts = ShapelyEx.get_points_uniform_in_polygon(poly1, 0.01,0.01)
+        # for cur_pt in edp_pts:
+        #     x, y = cur_pt[0]*0.001, cur_pt[1]*0.001 #Converting from mm to m
+        #     sim_sParams.add_electric_point_dipole([x,y, 1e-6], 1, [0,0,1])
+
+        sim_sParams.add_electric_point_dipole(self.adjoint_source_location, 1, [0,1,0])
+        # sim_sParams.add_electric_point_dipole([0,0,0], 0, [0,1,0])
+
+        cmsl.fine_mesh_around_comp_boundaries(['pad1', 'pad2'], minElementSize=10e-6, maxElementSize=50e-6)
+
+        cmsl.build_geom_mater_elec_mesh(skip_meshing=True, mesh_structure='Fine')
+
+        sim_sParams.set_freq_values([8.0333e9])
+        # cmsl.plot()
+        sim_sParams.run()
+        #sim_sParams.eval_field_at_pts('E', np.array([[0,0,0]]))
+
+        # get field at points
+
+    def evaluate_design_derivative(self):
+        #computing A_p, and evaluating it at the same mesh points as the forward field
+        pass
+
+    def optimisation_process(self):
+        # initialise optimizer
+        # intialize training data log
+        # training loop:
+            # if training condition satisfied:
+                #break
+            #compute gradient:
+                # run forward calc
+                # compute adjoint source
+                # run adjoint calc
+                # compute A_p
+                # compute Gradient Function
+            # update parameters
+            # update training data log
+
+        # Return final design, training data
+
+        pass
+
+    
+
+    def figure_of_merit(self):
+        """
+        @TODO: Abstract this method away so that we can use different figures of merit.
+        """
 
 
 
