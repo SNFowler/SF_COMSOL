a71df333b2eab3dd048bd41c10bb621d68d688b3
S Fowler <94100649+SNFowler@users.noreply.github.com>
2025-10-17 16:54:25 +1000

    Got plotting working

--- adjoint_sim_sf/AdjointSolver.py
+++ adjoint_sim_sf/AdjointSolver.py
@@ -27,7 +27,7 @@ from SQDMetal.COMSOL.SimCapacitance import COMSOL_Simulation_CapMats
 from SQDMetal.COMSOL.SimRFsParameter import COMSOL_Simulation_RFsParameters
 
 from SQDMetal.Utilities.ShapelyEx import ShapelyEx
-from typing import Iterable
+from typing import Iterable, List
 from dataclasses import dataclass, fields, asdict
 
 from .ParametricDesign import ParametricDesign
@@ -35,15 +35,11 @@ from .Simulation import SimulationRunner
 from .Sources import Source 
 """
 @TODO: Naming confusion. "design" in this code refers to both the qiskit design and to an instance of ParametricDesign class.
+@TODO: Account for shifting boundary when evaluating 
+@TODO: Account for changes in loss directly due to design parameters.
 """    
 
 class AdjointEvaluator:
-    """
-    @TODO: Units are not consistent between parameters, COMSOL simulation and QiskitMetal. 
-    @TODO: Velocity units wrong.
-    """
-
-
     def __init__(self, parametric_designer: ParametricDesign, config: dict = None):
         if COMSOL_Model._engine is None:
             raise RuntimeError(
@@ -57,12 +53,18 @@ class AdjointEvaluator:
         self.freq_value = 8e9
         self.param_perturbation = np.array([1e-5])          # in mm
         self.fwd_source_strength = 1e-2
+        self.silicon_base_area = 3.6E-7                     # in m, 600um*600um
 
         self.num_adj_sample_points = 20
+        self.source_vertical_displacement = 100e-6 # sources can't be at the surface exactly.
         
-        self.fwd_source_locations = np.array([[300e-6, 300e-6, 100e-6], [-200e-6, 400e-6, 100e-6]])  # in meters
-        self.jj_adjoint_sources = [
-            Source(np.array([0, 0, 100e-6]), np.array([0, 1, 0]))
+        self.fwd_source_locations = np.array([[300e-6, 300e-6, self.source_vertical_displacement], [-200e-6, 400e-6, self.source_vertical_displacement]])  # in meters
+        self.current_adjoint_sources = [
+            Source(np.array([0, 0, self.source_vertical_displacement]), np.array([0, 1, 0]))
+        ]
+
+        self.canonical_jj_adjoint_source = [
+            Source(np.array([0, 0, self.source_vertical_displacement]), np.array([0, 1, 0]))
         ]
         
         
@@ -83,12 +85,12 @@ class AdjointEvaluator:
         return self.sim_runner.run_forward(design, sources)
 
     def _adjoint_calculation(self, design, fwd_sparams):
-        str_values = self._adjoint_strength(fwd_sparams, self.jj_adjoint_sources)
+        str_values = self._adjoint_strength(fwd_sparams, self.current_adjoint_sources)
         
         # Create new Source objects with computed strengths
         sources = [
             Source(src.location, src.direction, strength)
-            for src, strength in zip(self.jj_adjoint_sources, str_values)
+            for src, strength in zip(self.current_adjoint_sources, str_values)
         ]
         
         return self.sim_runner.run_adjoint(design, sources)
@@ -118,20 +120,28 @@ class AdjointEvaluator:
         return adj_strength
   
     def evaluate(self, params: np.ndarray, perturbation_magnitude, 
-                 verbose=False,
+                 verbose=True,
                  objective_type: str = "jj",
                  fwd_sparams = None, adj_sparams = None): #optional for testing
         """
         Run forward + adjoint sims for given param specification, return (grad, loss). 
         grad is a complex number.
         Boundary velocity version
+
+        @TODO Right now 
         """
         match objective_type.lower():
             case "jj":
-                self.jj_adjoint_sources = self.get_JJ_adjoint_sources()
+                self.current_adjoint_sources = self.get_JJ_adjoint_sources()
             case "epr":
-                self.jj_adjoint_sources = self.get_epr_adjoint_sources(params, self.num_adj_sample_points, seed = self.random_seed)
-            
+                self.current_adjoint_sources = self.get_epr_adjoint_sources(params, self.num_adj_sample_points, seed = self.random_seed)
+            case "manual":
+                pass # used for testing, leaves sources unchanged. 
+            case _:
+                raise ValueError(f"Invalid objective type: {objective_type}")
+
+        if verbose:
+            print(f"{objective_type}: {self.current_adjoint_sources}")
 
         qk_design = self.parametric_designer.build_qk_design(params)
 
@@ -158,12 +168,12 @@ class AdjointEvaluator:
 
             grad_vec[basis_index] = grad
         
-        locations = np.array([src.location for src in self.jj_adjoint_sources])
+        locations = np.array([src.location for src in self.current_adjoint_sources])
         E_at_targets = self.sim_runner.eval_field_at_pts(fwd_sparams, 'E', locations)
 
         loss = 0
         #extract the relevant loss 
-        directions = np.array([src.direction for src in self.jj_adjoint_sources])
+        directions = np.array([src.direction for src in self.current_adjoint_sources])
         E_projected = np.sum(E_at_targets * directions, axis=-1)  # [n_sources]
         loss = -np.sum(np.abs(E_projected)**2)
 
@@ -174,11 +184,14 @@ class AdjointEvaluator:
         return grad_vec, loss
     
     def evaluate_multi_objective(self, params, perturbation_magnitude, 
-                                w_jj=0.5, w_epr=0.5):
+                                w_jj=0.5, w_epr=0.5,
+                                verbose = True):
         # Run forward sim once
         qk_design = self.parametric_designer.build_qk_design(params)
         fwd_sparams = self._fwd_calculation(qk_design)
        
+
+        
         grad_jj, loss_jj = 0, 0 
         grad_epr, loss_epr= 0, 0
         # JJ objective
@@ -197,6 +210,7 @@ class AdjointEvaluator:
         total_grad = w_jj * grad_jj + w_epr * grad_epr
         total_loss = w_jj * loss_jj + w_epr * loss_epr
         
+        
         return total_grad, total_loss, (loss_jj, loss_epr), (grad_jj, grad_epr)
 
     def save(self):
@@ -207,10 +221,8 @@ class AdjointEvaluator:
         WIP: In the future will facilitate non-boundary velocity methods
         """
         raise NotImplementedError
-
-    def _convert_unit(self, coord):
-        # @TODO: Get this working for np.array and multidimensional coord.
-        return coord * self.param_to_sim_scale
+    
+    
 
 
     def _calc_adjoint_forward_product(self, 
@@ -221,7 +233,7 @@ class AdjointEvaluator:
                                       adjoint_rotation: float = 0):
         
 
-        dr = 0.005 # mm
+        dr = 5e-6 # m
          #TODO: extract this from the boundary field intelligently, or pass it from/to parametric designer -> polygon constructor
 
         running_sum = 0
@@ -231,16 +243,21 @@ class AdjointEvaluator:
 
         #TODO: Vectorise this entire process and inner product for efficiency.
 
-        flat_boundary_velocity_field =  [v for multipoly_velocities in boundary_velocity_field for v in multipoly_velocities[0]]
-        flat_reference_coord = [coord_pair for multipoly_boundary_coord in reference_coord for coord_pair in multipoly_boundary_coord[0]]
+        flat_boundary_velocity_field = [
+                                        v for poly_vels in boundary_velocity_field
+                                        for ring_vels in poly_vels
+                                        for v in ring_vels
+                                        ]
+        flat_reference_coord =          [
+                                    xy for poly_refs in reference_coord
+                                    for ring_refs in poly_refs
+                                    for xy in ring_refs
+                                        ]
+        
 
         phase = np.exp(1j * float(adjoint_rotation))
 
         for v, (r1, r2) in zip(flat_boundary_velocity_field, flat_reference_coord):
-            v = self._convert_unit(v)
-            r1 = self._convert_unit(r1)
-            r2 = self._convert_unit(r2)
-
             # complete the forward and adjoint vectors. Note that the adjoint vector is actually the complex conjugate (but not the transpose) of the expected adjoint vector.
 
             local_fwd_vec =     self.sim_runner.eval_field_at_pts(fwd_sparams, 'E', [[r1, r2, r3]])
@@ -286,20 +303,91 @@ class AdjointEvaluator:
         plt.scatter(x[plane_inds], y[plane_inds], c=np.clip(np.abs(Ez[plane_inds]), 0,1e14))
 
     def get_JJ_adjoint_sources(self):
-        return self.jj_adjoint_sources
+        return self.canonical_jj_adjoint_source
     
-    def get_epr_adjoint_sources(self, params, n, seed=None):
+    def get_epr_adjoint_sources(self, params, n, interface = "SA", seed=None):
         """
         Returns a random sample of points above the design region without strengths
         """
+        iface = interface.upper()
+        assert iface in {"MA", "SA"}, f"Invalid interface '{iface}'" 
+        
         vertical_displacement = 1e-6
         orientation = np.array([0,0,1])
-        points_2d = self.parametric_designer.sample_interior_points(params, n, seed=seed)
-        
+        if iface == "MA":
+            points_2d = self.parametric_designer.sample_MA_points(params, n, seed=seed)
+        if iface == "SA":
+            points_2d = self.parametric_designer.sample_SA_points(params, n, seed=seed)
+            
         sources = [Source(np.array([pt[0], pt[1], vertical_displacement]), orientation) for pt in points_2d]
 
         return sources
     
+    # -----
+    # Utilities: 
+    #   - 1. geometry
+    #   - 2. updating/outputting params
+    # -----
+
+    # - 1. geometry
+    
+    def _get_metal_area(self, params: np.ndarray) -> float:
+        """
+        Area of the pad geometry
+        """
+        area_m2 = self.parametric_designer.get_interior_area(params)
+
+        return area_m2
+    
+    def _get_silicon_area(self, params: np.ndarray) -> float:
+        # the original silicon area minus the pads area.
+        return self.silicon_base_area - self._get_metal_area(params)
+    
+    def plot_sources(self, sources: List[Source], arrow_scale = 0.25):
+
+        # unit conversion
+        conversion = 1e4
+
+        xs = np.array([s.location[0] for s in sources])
+        ys = np.array([s.location[1] for s in sources])
+        zs = np.array([s.location[2] for s in sources])
+        us = np.array([s.direction[0] for s in sources])
+        vs = np.array([s.direction[1]  for s in sources])
+        ws = np.array([s.direction[2]  for s in sources])
+
+        fig = plt.figure(figsize=(10, 4))
+        ax1 = fig.add_subplot(1, 2, 1)
+        ax2 = fig.add_subplot(1, 2, 2, projection="3d")
+
+        ax1.scatter(xs, ys, s=20)
+        ax1.set_xlabel("x (m)"); ax1.set_ylabel("y (m)"); ax1.set_title("Source XY positions")
+        ax1.axis("equal")
+        
+
+        #rescale for the quiver plot to work properly
+        xs = np.array([s.location[0]*conversion for s in sources])
+        ys = np.array([s.location[1]*conversion for s in sources])
+        zs = np.array([s.location[2]*conversion for s in sources])
+        
+        ax2.quiver(xs, ys, zs, us, vs, ws, length=arrow_scale, arrow_length_ratio=0.2, linewidth=1)
+       
+        ax2.set_xlabel("x")
+        ax2.set_ylabel("y")
+        ax2.set_zlabel("z")
+        ax2.set_title("Source directions")
+
+        ax2.set_xlim([-4, 4])
+        ax2.set_ylim([-4, 4])
+        ax2.set_zlim([0, 8])
+
+        ax2.set_box_aspect([1, 1, 1])
+
+
+        return fig, (ax1, ax2)
+
+
+    # - 2. updating/outputting params
+
     def update_params(self, config_dict):
         for k, v in config_dict.items():
             if not hasattr(self, k):
